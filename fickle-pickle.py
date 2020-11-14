67db999b8be83286bbdfb34fbb2f2ac3122ff375
Ryan Lake <relh6r@umsystem.edu>
2020-11-14 14:38:39 -0600

    Added blinking

--- fickle-pickle.py
+++ fickle-pickle.py
@@ -1,38 +1,33 @@
 from tkinter import *
 import urllib.request
-
+import random
+import time
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
-
-chatbot = ChatBot(
-        "Ron",
-        storage_adapter='chatterbot.storage.SQLStorageAdapter',
-        logic_adapters=[
-            'chatterbot.logic.BestMatch'
-        ],
-        database_uri='sqlite:///database.db'
-        )
-
-trainer = ChatterBotCorpusTrainer(chatbot)
-
-trainer.train("chatterbot.corpus.english")
 
 #Closes the application
 def close():
     root.destroy()
 
-#18 characters
+#Update the user text box and ai text box
 def converse(event=None):
+    global emotion
     newUserText = editOutput(user_in.get())
     bot_response = chatbot.get_response(user_in.get())
     newAIText = editOutput(bot_response.text)
-    user_in.delete(0,END)
     user_out.delete("1.0","end")
     user_out.insert(END, newUserText, "center")
     ai_out.delete("1.0","end")
     ai_out.insert(END, newAIText, "center")
+    emotion = random.randint(0,2)
+    frame = 0
+    front = Label(root, image=animations[emotion][frame], bg="#f3f7d6")
+    front.place(x=400,y=0)
+    front.lower(ai_out)
+    user_in.delete(0,END)
 
+#Make the output strings fit the text boxes
 def editOutput(output):
     toEdit = output[:]
     toReturn = ""
@@ -50,9 +45,52 @@ def editOutput(output):
             toEdit = toEdit[1:]
     toReturn += temp
     return toReturn
-                
-            
+
+#Part of blinking animation
+def frame0():
+    frame = 0
+    front = Label(root, image=animations[emotion][frame], bg="#f3f7d6")
+    front.place(x=400,y=0)
+    front.lower(ai_out)
+    root.after(random.randint(500,3000), frame1)
+
+#Part of blinking animation
+def frame1():
+    frame = 1
+    front = Label(root, image=animations[emotion][frame], bg="#f3f7d6")
+    front.place(x=400,y=0)
+    front.lower(ai_out)
+    root.after(50, frame2)
+
+#Part of blinking animation
+def frame2():
+    frame = 2
+    front = Label(root, image=animations[emotion][frame], bg="#f3f7d6")
+    front.place(x=400,y=0)
+    front.lower(ai_out)
+    root.after(50, frame1Back)
+
+#Part of blinking animation
+def frame1Back():
+    frame = 1
+    front = Label(root, image=animations[emotion][frame], bg="#f3f7d6")
+    front.place(x=400,y=0)
+    front.lower(ai_out)
+    root.after(50, frame0)
     
+chatbot = ChatBot(
+        "Ron",
+        storage_adapter='chatterbot.storage.SQLStorageAdapter',
+        logic_adapters=[
+            'chatterbot.logic.BestMatch'
+        ],
+        database_uri='sqlite:///database.db'
+        )
+
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train("chatterbot.corpus.english")
+
 urllib.request.urlretrieve("https://raw.githubusercontent.com/ryanelake/fickle-pickle/main/icon.ico", "icon.ico")
 urllib.request.urlretrieve("https://raw.githubusercontent.com/ryanelake/fickle-pickle/main/back_pickle.png", "back.png")
 urllib.request.urlretrieve("https://raw.githubusercontent.com/ryanelake/fickle-pickle/main/Happy.png", "happy_base.png")
@@ -72,10 +110,15 @@ root.iconbitmap("icon.ico")
 root.configure(bg='#f3f7d6')
 root.bind('<Return>', converse)
 
+emotion = random.randint(0,2)
+frame = 0
 back = PhotoImage(file="back.png")
 Label(root, image=back, bg="#f3f7d6").place(x=-50,y=200)
-front = PhotoImage(file="happy_half.png")
-Label(root, image=front, bg="#f3f7d6").place(x=400,y=0)
+animations = [[PhotoImage(file="happy_base.png"), PhotoImage(file="happy_half.png"), PhotoImage(file="happy_close.png")],
+         [PhotoImage(file="sad_base.png"), PhotoImage(file="sad_half.png"), PhotoImage(file="sad_close.png")],
+         [PhotoImage(file="mad_base.png"), PhotoImage(file="mad_half.png"), PhotoImage(file="mad_close.png")]]
+front = Label(root, image=animations[emotion][frame], bg="#f3f7d6")
+front.place(x=400,y=0)
 user_in = Entry(width=58)
 user_in.place(x=5,y=475)
 temp = Button(text="Enter", width = 7,command=converse)
@@ -88,5 +131,7 @@ user_out.place(x=195,y=260)
 ai_out = Text(root, height=5, width = 19, bd=0)
 ai_out.tag_configure("center",justify='center')
 ai_out.place(x=450,y=20)
-           
+
+root.after(0, frame0)
+
 root.mainloop()
