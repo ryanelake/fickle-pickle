66fc668ca4f1d7b6c9a43148a3c3b540d80293cf
Ryan Lake <relh6r@umsystem.edu>
2020-11-13 22:14:51 -0600

    added pickle back

--- fickle-pickle.py
+++ fickle-pickle.py
@@ -24,24 +24,26 @@ def close():
     root.destroy()
 
 #Gets text in the box and prints it (for now)
-def update(event=None):
+def converse(event=None):
     print("User: ", user_in.get())
     bot_response = chatbot.get_response(user_in.get())
     print("Computer: ", bot_response)
     user_in.delete(0,len(user_in.get()))
     
 urllib.request.urlretrieve("https://raw.githubusercontent.com/ryanelake/fickle-pickle/main/icon.ico", "icon.ico")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/ryanelake/fickle-pickle/main/back_pickle.png", "back.png")
 root = Tk()
 root.title("The Fickle Pickle")
 root.geometry('750x500')
 root.iconbitmap("icon.ico")
 root.configure(bg='blue')
+root.bind('<Return>', converse)
 
-root.bind('<Return>', update)
-
+image = PhotoImage(file="back.png")
+Label(root, image=image, bg="blue").place(x=-130,y=200)
 user_in = Entry(width=100)
 user_in.place(x=5,y=475)
-temp = Button(text="Enter", width = 7,command=update)
+temp = Button(text="Enter", width = 7,command=converse)
 temp.place(x=620,y=470)
 temp = Button(text="Quit", width = 6, command=close)
 temp.place(x=690,y=470)
