895f30a863a6dc8caccc8e6ec66db54fb683913d
Ryan Lake <relh6r@umsystem.edu>
2020-11-13 19:08:53 -0600

    Changed window icon

--- fickle-pickle.py
+++ fickle-pickle.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import urllib.request
 
 #Closes the application
 def close():
@@ -9,11 +10,13 @@ def update(event=None):
     print(user_in.get())
     user_in.delete(0,len(user_in.get()))
     
-
+urllib.request.urlretrieve("https://raw.githubusercontent.com/ryanelake/fickle-pickle/main/icon.ico", "icon.ico")
 root = Tk()
 root.title("The Fickle Pickle")
 root.geometry('750x500')
+root.iconbitmap("icon.ico")
 root.configure(bg='blue')
+
 root.bind('<Return>', update)
 
 user_in = Entry(width=100)
