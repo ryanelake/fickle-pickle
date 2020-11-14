1bae758ce53ed4b66bc6fd44d13b636a7e91d97c
Ryan Lake <relh6r@umsystem.edu>
2020-11-13 18:49:44 -0600

    Basic UI

---
+++ fickle-pickle.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+#Closes the application
+def close():
+    root.destroy()
+
+#Gets text in the box and prints it (for now)
+def update(event=None):
+    print(user_in.get())
+    user_in.delete(0,len(user_in.get()))
+    
+
+root = Tk()
+root.title("The Fickle Pickle")
+root.geometry('750x500')
+root.configure(bg='blue')
+root.bind('<Return>', update)
+
+user_in = Entry(width=100)
+user_in.place(x=5,y=475)
+temp = Button(text="Enter", width = 7,command=update)
+temp.place(x=620,y=470)
+temp = Button(text="Quit", width = 6, command=close)
+temp.place(x=690,y=470)
+           
+root.mainloop()
